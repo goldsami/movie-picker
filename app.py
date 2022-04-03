bcf7dd37c34e450d92d6fcfde0684549b591845f
goldsami <zolotarenkomax@gmail.com>
2022-04-03 13:22:08 +0300

    feat: get random movie from watchlist feature added

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import csv
+import random
+
 import requests as requests
-from flask import Flask
+from flask import Flask, jsonify
 from bs4 import BeautifulSoup
 import json
 
 app = Flask(__name__)
 
 
-@app.route('/<userid>', methods=['POST'])
+@app.route('/<userid>', methods=['POST', 'GET'])
 def hello_world(userid):
     with open('data.json', 'r') as file:
         json_str = file.read()
@@ -21,7 +24,14 @@ def hello_world(userid):
             file.write(json.dumps({
                 "listId": list_id
             }))
-    return list_id
+    r2 = requests.get(f'https://www.imdb.com/list/{list_id}/export')
+    r3 = csv.reader(r2.text.split('\n'))
+    r4 = list(r3)[1:]
+    r5 = random.choice(r4)
+    return jsonify({
+        "name": r5[5],
+        "url": r5[6]
+    })
 
 
 if __name__ == '__main__':
